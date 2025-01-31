a41b02495c927219a41e71602ee829f566ec7b74
sohanxsahoo <sohanxsahoo@gmail.com>
2025-01-31 20:08:58 +0530

    first commit

---
+++ lib/db.ts
@@ -0,0 +1,40 @@
+import { ifError } from 'assert';
+import mongoose from 'mongoose'
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+    throw new Error("uri not defined");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: true,
+            maxPoolSize: 10
+        };
+
+        cached.promise = mongoose
+            .connect(MONGODB_URI, opts)
+            .then(() => mongoose.connection)
+    }
+
+    try {
+        cached.conn = await cached.promise
+    } catch (error) {
+        cached.promise = null
+        throw new Error("check db");
+    }
+
+    return cached.conn
+}
